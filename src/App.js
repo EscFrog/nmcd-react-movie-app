614ce41cb85f431458e629b067c427a2f0eccb87
Walter Park <neodust223@gmail.com>
2024-09-09 11:04:14 +0900

    HEAD 태그 내용 교체 실험

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Helmet>
+        <title>Escfrog's Movie App</title>
+      </Helmet>
+      <Router>
+        <Switch>
+          <Route path="/movie/:id">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
